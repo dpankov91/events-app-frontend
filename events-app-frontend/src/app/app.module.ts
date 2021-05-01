ebfeacc6c0a9dc0facde009b91afa92595858757
dpankov91 <dpankov91@gmail.com>
2021-05-01 17:58:24 +0300

    navbar finished

--- events-app-frontend/src/app/app.module.ts
+++ events-app-frontend/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { CreateEventPageComponent } from './create-event-page/create-event-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WelcomePageComponent,
+    CreateEventPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
-    BsDropdownModule.forRoot(),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ events-app-frontend/src/app/create-event-page/create-event-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-event-page',
+  templateUrl: './create-event-page.component.html',
+  styleUrls: ['./create-event-page.component.scss']
+})
+export class CreateEventPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
